a5d737911b9dd382ede2ebf3530d41f3640da758
Tseka Luk <tseka@nebutra.com>
2026-08-12 23:57:54 +0800

    release: ship Carina v0.8.18

--- go/product/version_test.go
+++ go/product/version_test.go
@@ -9,7 +9,7 @@ func TestVersionIsReleaseSemver(t *testing.T) {
 	if !regexp.MustCompile(`^[0-9]+\.[0-9]+\.[0-9]+$`).MatchString(Version) {
 		t.Fatalf("product version %q is not release semver", Version)
 	}
-	if Version != "0.8.17" {
-		t.Fatalf("next release version = %s, want 0.8.17", Version)
+	if Version != "0.8.18" {
+		t.Fatalf("next release version = %s, want 0.8.18", Version)
 	}
 }
