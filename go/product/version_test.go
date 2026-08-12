39c3223eff6f2a0a2f74f136e35b888a773085c8
Tseka Luk <tseka@nebutra.com>
2026-08-12 12:55:50 +0800

    release: prepare Carina v0.8.16

--- go/product/version_test.go
+++ go/product/version_test.go
@@ -9,7 +9,7 @@ func TestVersionIsReleaseSemver(t *testing.T) {
 	if !regexp.MustCompile(`^[0-9]+\.[0-9]+\.[0-9]+$`).MatchString(Version) {
 		t.Fatalf("product version %q is not release semver", Version)
 	}
-	if Version != "0.8.15" {
-		t.Fatalf("next release version = %s, want 0.8.15", Version)
+	if Version != "0.8.16" {
+		t.Fatalf("next release version = %s, want 0.8.16", Version)
 	}
 }
--- sdk/go/conformance_test.go
+++ sdk/go/conformance_test.go
@@ -17,7 +17,7 @@ func TestRealDaemonConformance(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer c.Close()
-	info, err := c.Initialize("carina-sdk-go", "0.8.15")
+	info, err := c.Initialize("carina-sdk-go", "0.8.16")
 	if err != nil {
 		t.Fatal(err)
 	}
